519b910d602ceea5498ba3e50fee5f618171e10f
Peter Johnston <peterwj@cs.stanford.edu>
2016-01-10 16:52:20 -0800

    day4 updated for second star

--- day04.py
+++ day04.py
@@ -2,15 +2,25 @@
 
 import copy
 import hashlib
+from sys import argv as argv
 
-key = 'bgvyzdsv'
-i = 0
-while True:
-	hash = hashlib.md5()
-	hash.update(key.encode('utf-8'))
-	hash.update(str(i).encode('utf-8'))
-	if hash.hexdigest()[0:5] == '00000':
-		break
-	i = i+1
+def do_calcs(key, zerocount):
+    result = '0' * zerocount
+    i = 0
+    while True:
+        hash = hashlib.md5()
+        hash.update(key.encode('utf-8'))
+        hash.update(str(i).encode('utf-8'))
+        if hash.hexdigest()[0:zerocount] == result:
+            break
+        i = i+1
+    return i
 
-print(i)
+if __name__ == '__main__':
+    if len(argv) <= 1:
+        zerocount = 5
+    else:
+        zerocount = int(argv[1])
+
+    print(do_calcs('bgvyzdsv', zerocount))
+
